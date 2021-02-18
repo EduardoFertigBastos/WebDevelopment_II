6362b48970644a58da88109bea4bc7a165f9374f
EduardoFertigBastos <dudufbastos1@gmail.com>
2021-02-17 21:41:07 -0300

    TBody Class

---
+++ 02_10/lib/TBody_class.php
@@ -0,0 +1,29 @@
+<?php
+
+class TBody {
+    private $lista = array();
+    private $class;
+
+    public function __construct($class = '') {
+        $this->class  = $class;
+    }
+
+    public function addElementToTBody($array) 
+    {
+        $this->lista[] = $array;
+    }
+
+    public function __toString()
+    {
+        $tbody = '<tbody class="'.$this->class.'">';
+
+        foreach ($this->lista as $valor) {
+            $tbody .= $valor;
+        }
+        
+        $tbody .= "</tbody>";
+
+        return $tbody;
+    }
+
+}
