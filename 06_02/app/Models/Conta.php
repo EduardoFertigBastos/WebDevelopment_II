3c367ff3a49841a2947ee2bcf8db4bda183b610b
EduardoFertigBastos <dudufbastos1@gmail.com>
2021-06-03 21:06:12 -0300

    ContaController

--- 06_02/app/Models/Conta.php
+++ 06_02/app/Models/Conta.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Conta extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'instituition',
+        'price',
+        'payment'
+    ];
 }
